694886d2800b55c62087126e1e3bfd8764aa8e24
lê trần khánh duy <jin21cm2k4@gmail.com>
2024-05-29 23:28:12 +0700

    kdyy

---
+++ LtkdLesson06/Controllers/LtkdSongController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LtkdLesson06.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace LtkdLesson06.Controllers
+{
+    public class LtkdSongController : Controller
+    {
+        private static List<LtkdSong> ltkdSongs = new List<LtkdSong>()
+        {
+            new LtkdSong {Id=00020,LtkdTitle="Lê Trần Khánh Duy",LtkdAuthor="K22CNT3",LtkdArtist="NTU",LtkdYearRelease=2020 },
+            new LtkdSong {Id=1,LtkdTitle="Ha Quoc Thanh",LtkdAuthor="K22CNT3",LtkdArtist="NTU",LtkdYearRelease=2020 }
+        };
+        
+        // GET: LtkdSong
+        /// <summary>
+        /// Hien Thi danh sach bai hat
+        /// Author: Le Tran Khanh Duy
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult LtkdIndex()
+        {
+            return View(ltkdSongs);
+        }
+
+        //Get: LtkdCreate
+        /// <summary>
+        /// Form them moi bai hat
+        /// Author : Le Tran Khanh Duy
+        /// </summary>
+        /// <returns></returns>
+
+        public ActionResult LtkdCreate()
+        {
+            var LtkdSong = new LtkdSong();
+            return View(LtkdSong);
+        }
+    }
+}
---
+++ LtkdLesson06/Models/LtkdSong.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace LtkdLesson06.Models
+{
+    public class LtkdSong
+    {
+        [Key]
+        public int Id { get; set; } 
+        [Required(ErrorMessage ="Ltkd: Hay nhap tieu de")]
+        [DisplayName("Tieu De")]
+        public string LtkdTitle { get; set; }
+        [Required(ErrorMessage = "Ltkd: Hay nhap tac gia")]
+        [DisplayName("Tac Gia")]
+        public string LtkdAuthor { get; set; }
+        [Required(ErrorMessage = "Ltkd: Hay nhap nghe si")]
+        [StringLength (50,MinimumLength =2,ErrorMessage ="Ltkd: Ten nghe si co toi thieu 2 ki tu, toi da 50 ki tu")]
+        [DisplayName("Nghe Si")]
+        public string LtkdArtist { get; set; }
+        [Required(ErrorMessage = "Ltkd: Hay nhap nam xuat ban")]
+        [RegularExpression(@"[0-9]{4}",ErrorMessage ="Ltkd: Nam xuat ban phia co 2 ki tu so")]
+        [Range(1900,2024,ErrorMessage ="Nam xuat ban trong khoang 1900-2024")]
+        [DisplayName("Nam Xuat Ban")]
+        public int LtkdYearRelease { get; set; } 
+
+    }
+}
